492f19d9509768a5c19e339d054a8031034af8ac
Daniel James <daniel@thzinc.com>
2018-03-28 21:29:50 -0700

    (+semver: feature) Better error handling

--- src/Regulations.Gov.Archiver/Archiver.cs
+++ src/Regulations.Gov.Archiver/Archiver.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Akka.Actor;
 using Akka.Event;
@@ -41,23 +42,32 @@ namespace Regulations.Gov.Archiver
                         ResultsPerPage = 1000,
                         PageOffset = pageOffset,
                     };
-                    // if (postedStartDate.HasValue)
-                    // {
-                    //     query["pd"] = new[] { $"{postedStartDate:MM/dd/yy}-" };
-                    // }
 
                     logger.Info(string.Join("; ", query.Select(x => $"{x.Key} = {string.Join(", ", x.Value)}")));
 
                     var result = await apiClient.GetDocuments(query);
-                    logger.Info($"Got {result.Documents.Count} documents, {pageOffset}/{result.TotalNumRecords} total available");
+                    if (result.Documents == null || result.Documents.Count == 0)
+                    {
+                        logger.Info("Reached the end. Waiting a bit to get more.");
+                        Context.System.Scheduler.ScheduleTellOnce(TimeSpan.FromMinutes(1), Self, pageOffset, Self);
+                    }
+                    else
+                    {
+                        logger.Info($"Got {result.Documents.Count} documents, {pageOffset}/{result.TotalNumRecords} total available");
 
-                    indexManager.Tell(result.Documents);
-                    Self.Tell(pageOffset + result.Documents.Count);
+                        indexManager.Tell(result.Documents);
+                        Self.Tell(pageOffset + result.Documents.Count);
+                    }
                 }
-                catch (ApiException ae)
+                catch (ApiException ae) when ((int)ae.StatusCode == 429)
                 {
                     logger.Error(ae, "Caught API exception; waiting a bit to send resend request");
                     Context.System.Scheduler.ScheduleTellOnce(TimeSpan.FromMinutes(1), Self, pageOffset, Self);
+                }
+                catch (Exception e)
+                {
+                    logger.Error(e, "Caught other exception; retrying");
+                    Self.Tell(pageOffset);
                 }
             });
 
